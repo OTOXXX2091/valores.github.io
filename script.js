854df0b2e5c9f14c3f9e4f1f9be6beb81e4ed8d0
OTOXXX2091 <148468515+OTOXXX2091@users.noreply.github.com>
2023-10-19 08:54:19 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,275 @@
+// script.js 
+
+let questions = [ 
+	{ 
+		prompt: `Cual es el siguiente valor
+		         Actuamos de forma responsable y oportuna
+		         para cumplir los objetivos de la empresa`, 
+		options: [ 
+			"<Excelencia>", 
+			"<Integridad>", 
+			"<Disciplina>", 
+			 
+		], 
+		answer: "<Disciplina>", 
+	}, 
+
+	{ 
+		prompt: `Cual es el siguiente valor
+		         Realizamos nuestro trabajo con el más alto nivel de
+				  calidad, eficiencia y eficacia`, 
+		options: [ 
+			"Disciplina", 
+			"Excelencia", 
+			"Integridad", 
+			 
+		], 
+		answer: "Excelencia", 
+	}, 
+
+	{ 
+		prompt: `Cual es el siguiente valor
+		         Somos personas que trabajamos por un fin común, 
+		         para garantizar con excelencia y responsabilidad
+		         ambiental la seguridad energética del país`, 
+		options: [ 
+			"Trabajo en equipo", 
+			"Excelencia", 
+			"Respeto", 
+			 
+		], 
+		answer: "Trabajo en equipo", 
+	}, 
+
+	{ 
+		prompt: `Cual es el siguiente valor:
+		         Somos personas honestas y transparentes,
+				 con la Empresa y con la sociedad`, 
+		options: [
+			      "Respeto",
+		          "Integridad",
+				  "Excelencia",
+				], 
+		answer: "Integridad", 
+	}, 
+
+	{ 
+		prompt: `Cual es el siguiente valor:Trabajamos en un marco de relaciones interpersonales sanas
+		          basadas en la igualdad, 
+				  la tolerancia a las diferencias y con una comunicación asertiva.`, 
+		options: [ 
+			"Integridad", 
+			"Trabajo  en equipo", 
+			"Respeto", 
+			
+		], 
+		answer: "Respeto", 
+	}, 
+]; 
+
+// Get Dom Elements 
+
+let questionsEl = 
+	document.querySelector( 
+		"#questions"
+	); 
+let timerEl = 
+	document.querySelector("#timer"); 
+let choicesEl = 
+	document.querySelector("#options"); 
+let submitBtn = document.querySelector( 
+	"#submit-score"
+); 
+let startBtn = 
+	document.querySelector("#start"); 
+let nameEl = 
+	document.querySelector("#name"); 
+let feedbackEl = document.querySelector( 
+	"#feedback"
+); 
+let reStartBtn = 
+	document.querySelector("#restart"); 
+
+// Quiz's initial state 
+let currentQuestionIndex = 0; 
+let time = questions.length * 15; 
+let timerId; 
+
+// Start quiz and hide frontpage 
+
+function quizStart() { 
+	timerId = setInterval( 
+		clockTick, 
+		1000 
+	); 
+	timerEl.textContent = time; 
+	let landingScreenEl = 
+		document.getElementById( 
+			"start-screen"
+		); 
+	landingScreenEl.setAttribute( 
+		"class", 
+		"hide"
+	); 
+	questionsEl.removeAttribute( 
+		"class"
+	); 
+	getQuestion(); 
+} 
+
+// Loop through array of questions and 
+// Answers and create list with buttons 
+function getQuestion() { 
+	let currentQuestion = 
+		questions[currentQuestionIndex]; 
+	let promptEl = 
+		document.getElementById( 
+			"question-words"
+		); 
+	promptEl.textContent = 
+		currentQuestion.prompt; 
+	choicesEl.innerHTML = ""; 
+	currentQuestion.options.forEach( 
+		function (choice, i) { 
+			let choiceBtn = 
+				document.createElement( 
+					"button"
+				); 
+			choiceBtn.setAttribute( 
+				"value", 
+				choice 
+			); 
+			choiceBtn.textContent = 
+				i + 1 + ". " + choice; 
+			choiceBtn.onclick = 
+				questionClick; 
+			choicesEl.appendChild( 
+				choiceBtn 
+			); 
+		} 
+	); 
+} 
+
+// Check for right answers and deduct 
+// Time for wrong answer, go to next question 
+
+function questionClick() { 
+	if ( 
+		this.value !== 
+		questions[currentQuestionIndex] 
+			.answer 
+	) { 
+		time -= 10; 
+		if (time < 0) { 
+			time = 0; 
+		} 
+		timerEl.textContent = time; 
+		feedbackEl.textContent = `Incorecto!
+		${questions[currentQuestionIndex].answer}.`; 
+		feedbackEl.style.color = "red"; 
+	} else { 
+		feedbackEl.textContent = 
+			"correcto!"; 
+		feedbackEl.style.color = 
+			"green"; 
+	} 
+	feedbackEl.setAttribute( 
+		"class", 
+		"feedback"
+	); 
+	setTimeout(function () { 
+		feedbackEl.setAttribute( 
+			"class", 
+			"feedback hide"
+		); 
+	}, 2000); 
+	currentQuestionIndex++; 
+	if ( 
+		currentQuestionIndex === 
+		questions.length 
+	) { 
+		quizEnd(); 
+	} else { 
+		getQuestion(); 
+	} 
+} 
+
+// End quiz by hiding questions, 
+// Stop timer and show final score 
+
+function quizEnd() { 
+	clearInterval(timerId); 
+	let endScreenEl = 
+		document.getElementById( 
+			"quiz-end"
+		); 
+	endScreenEl.removeAttribute( 
+		"class"
+	); 
+	let finalScoreEl = 
+		document.getElementById( 
+			"score-final"
+		); 
+	finalScoreEl.textContent = time; 
+	questionsEl.setAttribute( 
+		"class", 
+		"hide"
+	); 
+} 
+
+// End quiz if timer reaches 0 
+
+function clockTick() { 
+	time--; 
+	timerEl.textContent = time; 
+	if (time <= 0) { 
+		quizEnd(); 
+	} 
+} 
+
+// Save score in local storage 
+// Along with users' name 
+
+function saveHighscore() { 
+	let name = nameEl.value.trim(); 
+	if (name !== "") { 
+		let highscores = 
+			JSON.parse( 
+				window.localStorage.getItem( 
+					"highscores"
+				) 
+			) || []; 
+		let newScore = { 
+			score: time, 
+			name: name, 
+		}; 
+		highscores.push(newScore); 
+		window.localStorage.setItem( 
+			"highscores", 
+			JSON.stringify(highscores) 
+		); 
+		alert( 
+			"Your Score has been Submitted"
+		); 
+	} 
+} 
+
+// Save users' score after pressing enter 
+
+function checkForEnter(event) { 
+	if (event.key === "Enter") { 
+		saveHighscore(); 
+		alert( 
+			"Your Score has been Submitted"
+		); 
+	} 
+} 
+nameEl.onkeyup = checkForEnter; 
+
+// Save users' score after clicking submit 
+
+submitBtn.onclick = saveHighscore; 
+
+// Start quiz after clicking start quiz 
+
+startBtn.onclick = quizStart;
